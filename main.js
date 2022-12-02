544a2d9fe54da09304b90f822c5134f7171716ff
jailtonj <jailtonpereira762@gmail.com>
2022-12-02 15:55:39 -0300

    links tenis

--- main.js
+++ main.js
@@ -8,19 +8,19 @@ function createGame() {
             </div>
         </div>
         <div class="card">
-            <a href="https://duesportes.com.br/produtos/tenis-air-jordan-1-retro-high-og-court-purple-1-0/" target="_blank">
+            <a href="https://importprodutosbr.com.br/produtos/tenis-nike-air-jordan-1-mid-black-white-noble-red/" target="_blank">
             <div class="images">
                     <img src="./pictures/icon=noble.svg" alt=""></a>
             </div>
         </div>
             <div class="card">
-                <a href="https://duesportes.com.br/produtos/tenis-air-jordan-1-retro-high-og-court-purple-1-0/" target="_blank">
+                <a href="https://www.world18kbr.com.br/produtos/air-jordan-1-high-court-purple/?srsltid=AYJSbAdfPe0C6FGAr7AQ_TpHEhUo2ssGx6E9yVaPaEisXXLdG6sNx-vo5eY" target="_blank">
                 <div class="images">
                 <img src="./pictures/air-jordan-8-retro-high-court-purple-w-1-400.svg" alt=""></a>
                 </div>
             </div>
             <div class="card">
-                <a href="https://duesportes.com.br/produtos/tenis-air-jordan-1-retro-high-og-court-purple-1-0/" target="_blank">
+                <a href="https://droper.app/d/160/Air_Jordan_1_Mid_Smoke_Grey?srsltid=AYJSbAe-uRJYUvDlJJa5gOH3-c75oALVyDZ_a1V9xOxvtT8_IvLjOkZLT_I" target="_blank">
                 <div class="images">
                 <img src="./pictures/air-jordan-7-mid-light-smoke-grey-gs-1-1000.svg" alt=""></a>
                 </div>
@@ -28,25 +28,25 @@ function createGame() {
     </div>
     <div class="app2">
             <div class="card">
-                <a href="https://duesportes.com.br/produtos/tenis-air-jordan-1-retro-high-og-court-purple-1-0/" target="_blank">
+                <a href="https://www.originalsaopaulo.com/produto/air-jordan-1-mid-grey-camo/?attribute_pa_tamanho-br=42&utm_source=Google+Shopping&utm_medium=cpc&utm_campaign=Original+Feed+Merchant&srsltid=AYJSbAd8QF8A1RlwgBd8jex8W9G4CC26JkBrLfnbqTKlFFu5ARDBqPmX0HI" target="_blank">
                 <div class="images">
                 <img src="./pictures/air-jordan-6-mid-grey-camo-1-1000 1.svg" alt=""></a>
                 </div>
             </div>
             <div class="card">
-                <a href="https://duesportes.com.br/produtos/tenis-air-jordan-1-retro-high-og-court-purple-1-0/" target="_blank">
+                <a href="https://sportswinner.com.br/produto/tenis-air-jordan-1-mid-dutch-green-2/" target="_blank">
                 <div class="images">
                 <img src="./pictures/air-jordan-5-mid-dutch-green-1-400.svg" alt=""></a>
                 </div>
             </div>
             <div class="card">
-                <a href="https://duesportes.com.br/produtos/tenis-air-jordan-1-retro-high-og-court-purple-1-0/" target="_blank">
+                <a href="https://droper.app/d/566/Air_Jordan_1_Mid_Carbon_Fiber" target="_blank">
                 <div class="images">
                 <img src="./pictures/air-jordan-4-mid-carbon-fiber-1-1000 1.svg" alt=""></a>
                 </div>
             </div>
             <div class="card">
-                <a href="https://duesportes.com.br/produtos/tenis-air-jordan-1-retro-high-og-court-purple-1-0/" target="_blank">
+                <a href="https://droper.app/d/1017/Air_Jordan_1_Mid_SE_Bright_Citrus" target="_blank">
                 <div class="images">
                 <img src="./pictures/air-jordan-3-mid-bright-citrus-1-1000 1.svg" alt=""></a>
                 </div>
@@ -54,6 +54,5 @@ function createGame() {
         </div>
     `
   }
-
   document.querySelector("#cards").innerHTML = 
   createGame()
