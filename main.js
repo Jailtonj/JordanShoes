6e6f7074ec39f9220fa713d1296ec954ba09315b
jailtonj <jailtonpereira762@gmail.com>
2022-12-02 11:03:38 -0300

    java script DOM

---
+++ main.js
@@ -0,0 +1,59 @@
+function createGame() {
+    return `
+    <div class="app1">
+        <div class="card">
+            <a href="https://duesportes.com.br/produtos/tenis-air-jordan-1-retro-high-og-court-purple-1-0/" target="_blank">
+            <div class="images">
+                    <img src="./pictures/air-jordan-1-high-zoom-cmft-tropical-twist.svg" alt=""></a>
+            </div>
+        </div>
+        <div class="card">
+            <a href="https://duesportes.com.br/produtos/tenis-air-jordan-1-retro-high-og-court-purple-1-0/" target="_blank">
+            <div class="images">
+                    <img src="./pictures/icon=noble.svg" alt=""></a>
+            </div>
+        </div>
+            <div class="card">
+                <a href="https://duesportes.com.br/produtos/tenis-air-jordan-1-retro-high-og-court-purple-1-0/" target="_blank">
+                <div class="images">
+                <img src="./pictures/air-jordan-8-retro-high-court-purple-w-1-400.svg" alt=""></a>
+                </div>
+            </div>
+            <div class="card">
+                <a href="https://duesportes.com.br/produtos/tenis-air-jordan-1-retro-high-og-court-purple-1-0/" target="_blank">
+                <div class="images">
+                <img src="./pictures/air-jordan-7-mid-light-smoke-grey-gs-1-1000.svg" alt=""></a>
+                </div>
+            </div>
+    </div>
+    <div class="app2">
+            <div class="card">
+                <a href="https://duesportes.com.br/produtos/tenis-air-jordan-1-retro-high-og-court-purple-1-0/" target="_blank">
+                <div class="images">
+                <img src="./pictures/air-jordan-6-mid-grey-camo-1-1000 1.svg" alt=""></a>
+                </div>
+            </div>
+            <div class="card">
+                <a href="https://duesportes.com.br/produtos/tenis-air-jordan-1-retro-high-og-court-purple-1-0/" target="_blank">
+                <div class="images">
+                <img src="./pictures/air-jordan-5-mid-dutch-green-1-400.svg" alt=""></a>
+                </div>
+            </div>
+            <div class="card">
+                <a href="https://duesportes.com.br/produtos/tenis-air-jordan-1-retro-high-og-court-purple-1-0/" target="_blank">
+                <div class="images">
+                <img src="./pictures/air-jordan-4-mid-carbon-fiber-1-1000 1.svg" alt=""></a>
+                </div>
+            </div>
+            <div class="card">
+                <a href="https://duesportes.com.br/produtos/tenis-air-jordan-1-retro-high-og-court-purple-1-0/" target="_blank">
+                <div class="images">
+                <img src="./pictures/air-jordan-3-mid-bright-citrus-1-1000 1.svg" alt=""></a>
+                </div>
+            </div>
+        </div>
+    `
+  }
+
+  document.querySelector("#cards").innerHTML = 
+  createGame()
